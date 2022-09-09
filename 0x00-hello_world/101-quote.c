05705453d508419a1e79560e4ff67222eabac498
Opeoluwa <onifadeomolara01@gmail.com>
2022-09-09 08:12:57 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,10 @@
 /**
  *main - Entry point
  *
- *Return:0
+ *Return: Always 0
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-						\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
